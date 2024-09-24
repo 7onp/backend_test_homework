1363ff5c87b2030d8f69e568ca49f14e5c7cf0a9
7onp <semyonpirogov@gmail.com>
2024-09-24 16:57:21 -0600

    Delete django_sprint1 directory

--- django_sprint1/blogicum/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from pages import views
-
-app_name = "pages"
-
-urlpatterns = [
-    path("about/", views.about, name="about"),
-    path("rules/", views.rules, name="rules"),
-]
